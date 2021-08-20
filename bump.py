27a7267122a757b3d10aa79e48fc22cfddb38bea
joepcodes <89229910+joepcodes@users.noreply.github.com>
2021-08-20 02:25:11 +0200

    Add files via upload

---
+++ bump.py
@@ -0,0 +1,15 @@
+import discord, os, time
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = "--", self_bot=True)
+
+@bot.event
+async def on_ready():
+    print("Auto Bump bot started!!")
+    while True:
+        channel = bot.get_channel(CHANNEL_ID)
+        await channel.send("!d bump")
+        print("bumped 1")
+        time.sleep(7500)
+
+bot.run("TOKEN", bot = False)
